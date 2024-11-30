cd74846fe1c72a8d1c3fbeddff38f6f0ae75703f
JAOUAD OUXHAIB s306971 <s306971@studenti.polito.it>
2024-11-30 10:22:22 +0100

    added tailwind theming

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -8,10 +8,12 @@ import { WordsService } from './words.service';
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <div class="bg-white flex flex-col p-4 rounded-lg shadow-lg">
+    <div class="bg-white flex flex-col p-4 gap-y-2 rounded-lg shadow-lg">
       @for (word of words; track $index) {
-      <button class="bg-zinc-200 p-2 rounded m-2 text-lg">
-        {{ word.word }}
+      <button
+        class="inline-flex items-center whitespace-nowrap rounded-xl text-base font-bold uppercase tracking-wide ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:bg-disabled disabled:text-disabled-foreground disabled:border-transparent bg-secondary/15 text-secondary border-2 border-secondary/80 hover:bg-secondary/20 px-4 h-auto w-full justify-start py-2 sm:max-lg:w-auto sm:max-lg:px-2 border-b-2"
+      >
+        {{ word.rank }}: {{ word.word }}
       </button>
       }
     </div>
